a1fa792a23d6dcf843a436b29e2016cdfc7a2af1
RCaracuel <rosa.caracuel@gmail.com>
2019-05-16 14:08:51 +0200

    Creación clase Conexión

---
+++ src/main/java/abonados/Conexion.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abonados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author rosa
+ */
+public class Conexion {
+    
+     private static final String SERVIDOR = "jdbc:mysql://192.168.56.101/";
+    private static final String NOMBRE_BASE_DATOS = "Parking";
+    private static final String USER = "rosa";
+    private static final String PASS = "1234";
+
+    private static Connection instancia = null; //sólo va a haber una conexión
+    
+    // Constructor privado no accesible desde otras clases
+    private Conexion() {
+
+    }
+
+    // Método de clase para acceder a la instancia del objeto Connection
+    public static Connection getInstance() {
+        // Si el objeto Connection no está creado, se crea
+        if (instancia == null) {
+            try {
+
+                // Se crea el objeto Connection	
+                instancia = DriverManager.getConnection(SERVIDOR + NOMBRE_BASE_DATOS, USER, PASS);
+
+                System.out.println("Conexión realizada con éxito.");
+
+            } catch (SQLException e) {
+
+                // Error en la conexión
+                System.out.println("Conexión fallida: " + e.getMessage());
+            }
+        }
+        return instancia;
+    }
+    
+}
