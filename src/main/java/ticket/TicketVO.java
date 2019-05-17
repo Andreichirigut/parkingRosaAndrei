e9ac240b9d31d5dafa41db388a4bf51558f71407
RCaracuel <rosa.caracuel@gmail.com>
2019-05-17 12:29:17 +0200

    Creación del paquete ticket y de la clase TicketVO, con atributos, métodos y constructores

---
+++ src/main/java/ticket/TicketVO.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ticket;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author rosa
+ */
+public class TicketVO {
+    
+    //Atributos
+    private int pin;
+    private String matricula;
+    private LocalDate fecha;
+    private int numeroPlaza;
+    
+    //Constructores
+
+    public TicketVO(int pin, String matricula, LocalDate fecha, int numeroPlaza) {
+        this.pin = pin;
+        this.matricula = matricula;
+        this.fecha = fecha;
+        this.numeroPlaza = numeroPlaza;
+    }
+
+    public TicketVO() {
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getNumeroPlaza() {
+        return numeroPlaza;
+    }
+
+    public void setNumeroPlaza(int numeroPlaza) {
+        this.numeroPlaza = numeroPlaza;
+    }
+
+    @Override
+    public String toString() {
+        return "TicketVO{" + "pin=" + pin + ", matricula=" + matricula + ", fecha=" + fecha + ", numeroPlaza=" + numeroPlaza + '}';
+    }
+    
+    
+}
