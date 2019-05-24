c186701d56dd4a4b4f3c8924416d8456f2940896
RCaracuel <rosa.caracuel@gmail.com>
2019-05-24 12:29:05 +0200

    Introduzco todas las plazas, 15 para cada tipo y modifico la llamada a los métodos también

--- src/main/java/plaza/Programa.java
+++ src/main/java/plaza/Programa.java
@@ -6,7 +6,6 @@
 package plaza;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +18,53 @@ public class Programa {
     public static void main(String[] args) {
         PlazasDAO daoPlaza = new PlazasDAO();
         List<PlazasVO> listaPlazas = new ArrayList<>();
-        listaPlazas.add(new PlazasVO(101, "Turismo ", false, 0.12));
-        listaPlazas.add(new PlazasVO(102, "Motocicleta", false, 0.08));
-        listaPlazas.add(new PlazasVO(103, "Caravana", false, 0.45));
-        listaPlazas.add(new PlazasVO(104, "Turismo ", false, 0.12));
-        listaPlazas.add(new PlazasVO(105, "Motocicleta", false, 0.08));
-        listaPlazas.add(new PlazasVO(106, "Caravana", false, 0.45));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Turismo"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Caravana"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        listaPlazas.add(new PlazasVO("Motocicleta"));
+        
+        
 
         try {
 
@@ -35,20 +75,20 @@ public class Programa {
             System.out.println("-------- Lista con datos recogidos desde la B.D -------------");
             nuevaLista.forEach(System.out::println);
             System.out.println("-----------------------------------------");
-            System.out.println("Plaza con primary key 102: ");
-            System.out.println(daoPlaza.findByPk(102));
+            System.out.println("Plaza con primary key 45: ");
+            System.out.println(daoPlaza.findByPk(42));
             System.out.println("-----------------------------------------");
-            System.out.println("Se va a borrar la plaza con pk 104");
+            System.out.println("Se va a borrar la plaza con pk 32");
             System.out.println("Nº centros borradas "
-                    + daoPlaza.deletePlaza(new PlazasVO(104, "Turismo", false, 0.12)));
+                    + daoPlaza.deletePlaza(32));
             System.out.println("-----------------------------------------");
             nuevaLista = daoPlaza.getAll();
             System.out.println("-------- Lista con datos recogidos desde la B.D despues de borrar un centro -------------");
             nuevaLista.forEach(System.out::println);
             System.out.println("-----------------------------------------");
-            System.out.println("Modificación de la plaza con pk 105");
+            System.out.println("Modificación de la plaza con pk 10");
             System.out.println("Nº plazas modificados "
-                    + daoPlaza.updatePlaza(105, new PlazasVO(106, "Caravana", true, 0.45)));
+                    + daoPlaza.updatePlaza(10, new PlazasVO("Caravana")));
             System.out.println("-----------------------------------------");
             nuevaLista = daoPlaza.getAll();
             System.out.println("-------- Lista con datos recogidos desde la B.D despues de modificar una persona -------------");
