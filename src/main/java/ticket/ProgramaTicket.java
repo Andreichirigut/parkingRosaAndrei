68187367a4f75d0fb0329bb2d6bb8efcdd39f429
AndreiChirigut <andreichirigut07@gmail.com>
2019-05-28 21:26:04 +0200

    Ajuste en el main

--- src/main/java/ticket/ProgramaTicket.java
+++ src/main/java/ticket/ProgramaTicket.java
@@ -18,10 +18,10 @@ public class ProgramaTicket {
 
     public static void main(String[] args) {
         TicketDAO ticket=new TicketDAO();
-        TicketVO prueba = new TicketVO("789775");
-        TicketVO prueba2 = new TicketVO("789885");
-        TicketVO prueba3 = new TicketVO("789776");
-        TicketVO prueba4 = new TicketVO("789887");
+        TicketVO prueba = new TicketVO("789775", 5);
+        TicketVO prueba2 = new TicketVO("789885", 6);
+        TicketVO prueba3 = new TicketVO("789776", 7);
+        TicketVO prueba4 = new TicketVO("789887", 8);
         
         prueba.setPin("000000");
         prueba2.setPin("111111");
@@ -42,24 +42,24 @@ public class ProgramaTicket {
             List<TicketVO> nuevaLista = ticket.getAll();
             System.out.println("-------- Lista con datos recogidos desde la B.D -------------");
             nuevaLista.forEach(System.out::println);
-            System.out.println("-----------------------------------------");
-            System.out.println("Ticket con primary key 1: ");
-            System.out.println(ticket.findByPk("000000"));
-            System.out.println("-----------------------------------------");
-            System.out.println("Se va a borrar el ticket con pk 000000");
-            System.out.println("Nº personas borradas " + ticket.delete_Tickets(prueba));
-            System.out.println("-----------------------------------------");
-            nuevaLista = ticket.getAll();
-            System.out.println("-------- Lista con datos recogidos desde la B.D despues de borrar un ticket-------------");
-            nuevaLista.forEach(System.out::println);
-            System.out.println("-----------------------------------------");
-            System.out.println("Modificación del ticket  con pk 111111");
-            System.out.println("Nº Personas modificadas " + ticket.updateTicket("111111", prueba2));
-            System.out.println("-----------------------------------------");
-            nuevaLista = ticket.getAll();
-            System.out.println("-------- Lista con datos recogidos desde la B.D despues de modificar un ticket -------------");
-            nuevaLista.forEach(System.out::println);
-            System.out.println("-----------------------------------------");
+//            System.out.println("-----------------------------------------");
+//            System.out.println("Ticket con primary key 1: ");
+//            System.out.println(ticket.findByPk("000000"));
+//            System.out.println("-----------------------------------------");
+//            System.out.println("Se va a borrar el ticket con pk 000000");
+//            System.out.println("Nº personas borradas " + ticket.delete_Tickets(prueba));
+//            System.out.println("-----------------------------------------");
+//            nuevaLista = ticket.getAll();
+//            System.out.println("-------- Lista con datos recogidos desde la B.D despues de borrar un ticket-------------");
+//            nuevaLista.forEach(System.out::println);
+//            System.out.println("-----------------------------------------");
+//            System.out.println("Modificación del ticket  con pk 111111");
+//            System.out.println("Nº Personas modificadas " + ticket.updateTicket("111111", prueba2));
+//            System.out.println("-----------------------------------------");
+//            nuevaLista = ticket.getAll();
+//            System.out.println("-------- Lista con datos recogidos desde la B.D despues de modificar un ticket -------------");
+//            nuevaLista.forEach(System.out::println);
+//            System.out.println("-----------------------------------------");
 //            System.out.println("Ejecución del procedimiento almacenado");
 //            System.out.println("Se cambia Juanillo dinamita por Hola holita");
 //            System.out.println("Nombres cambiados " + abo.cambiarNombres("Hola holita", "Juanillo dinamita"));
