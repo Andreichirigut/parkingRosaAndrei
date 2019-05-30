1b29d9dd8347e5cde83624006bfa26a7b63963d5
RCaracuel <rosa.caracuel@gmail.com>
2019-05-30 10:18:23 +0200

    Completando el menú

--- src/main/java/aplicacion/Central.java
+++ src/main/java/aplicacion/Central.java
@@ -5,13 +5,19 @@
  */
 package aplicacion;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
  *
  * @author andrei
  */
 public class Central {
-    public static void ejecutarOrden(Comando comando) throws SQLException{
+
+    public static void ejecutarOrden(Comando comando) throws SQLException, IOException {
+        Scanner teclado = new Scanner(System.in);
+        int opcion = 0;
         switch (comando) {
             case DEPOSITAR_VEHICULO:
                 GestionVehiculos.altaCliente();
@@ -44,26 +50,77 @@ public class Central {
                 GestionVehiculos.bajaAbonado();
                 break;
             case CONSULTAR_CADUCIDAD_ABONADOS:
-                
+                System.out.println("1.-Consultar caducidad según mes");
+                System.out.println("2.-Consultar próximos 10 días");
+                opcion=teclado.nextInt();
+                switch (opcion) {
+                    case 1:
+                        GestionVehiculos.caducidad();
+                        break;
+                    case 2: GestionVehiculos.ultimosDias();
+                    default:
+                        throw new AssertionError();
+                }
+
                 break;
             case CONSULTAR_CADUCIDAD_ABONADOS_10DIAS:
-               GestionVehiculos.altaAbonado();
+                GestionVehiculos.altaAbonado();
                 break;
             case CREAR_COPIA_SEGURIDAD:
                 System.out.println("CREAR COPIA DE SEGURIDAD");
                 System.out.println("1.-De Clientes");
-                System.out.println("2.-De Tickets");
-                System.out.println("3.-De");
+                System.out.println("2.-De Vehiculos");
+                System.out.println("3.-De Plazas");
+                System.out.println("4.-De tickets");
+
+                opcion = teclado.nextInt();
+                switch (opcion) {
+                    case 1:
+                        GestionVehiculos.copiaAbonados();
+                        break;
+                    case 2:
+                        GestionVehiculos.copiasVehiculos();
+                        break;
+                    case 3:
+                        GestionVehiculos.copiasPlazas();
+                        break;
+                    case 4:
+                        GestionVehiculos.copiasTickets();
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+
                 break;
             case RESTAURAR_COPIA_SEGURIDAD:
-                System.out.println("");
+                System.out.println("1.-Restaurar abonados");
+                System.out.println("2.-Restaurar Vehiculos");
+                System.out.println("3.-Restaurar plazas");
+                System.out.println("4.-Restaurar tickets");
+                opcion=teclado.nextInt();
+                switch (opcion) {
+                    case 1:
+                        GestionVehiculos.restaurarAbonados();
+                        break;
+                    case 2:
+                        GestionVehiculos.restaurarVehiculos();
+                        break;
+                    case 3:
+                        GestionVehiculos.restaurarPlazas();
+                        break;
+                    case 4:
+                        GestionVehiculos.restaurarTickets();
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
                 break;
             default:
                 throw new AssertionError();
         }
     }
-    
-    public static void main(String[] args) throws SQLException {
+
+    public static void main(String[] args) throws SQLException, IOException {
         ejecutarOrden(Menu.menu());
     }
 }
